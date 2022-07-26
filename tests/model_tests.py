e99ecc6620d7cb388ccc918aab0de7cc8b0392fd
JRogulski <jakubrogulski01@gmail.com>
2022-07-26 16:49:47 +0200

    added umap plot and metrics system

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -1,8 +1,12 @@
 from src import train
 from src import data_proccesing
 import joblib
+from src import config
+from src import data_visualization
 train_scaled = data_proccesing.train_scaled
 model_bgm = joblib.load('../models/BayesianGaussianMixture')
 
 X, y = train.predict(model_bgm,train_scaled)
-train.submission(y)
+data_visualization.umapp(train_scaled,y)
+#config.usnupervised_metrics(y, train_scaled, '../models/BayesianGaussianMixture')
+#train.submission(y)
