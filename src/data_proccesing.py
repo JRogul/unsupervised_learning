f793f28d75a205fec5c672c84b9ba48e5c2447ca
JRogulski <jakubrogulski01@gmail.com>
2022-07-26 10:27:37 +0200

    simple baseline

--- src/data_proccesing.py
+++ src/data_proccesing.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 train_df = pd.read_csv(config.TRAIN_DATA)
 feature_selector = VarianceThreshold(threshold=1.5)
 
-
+#picked by inspecting variance threshold plot
 best_data = ['f_07', 'f_08', 'f_09', 'f_10', 'f_11',
              'f_12', 'f_13', 'f_22', 'f_23', 'f_24',
              'f_25', 'f_26', 'f_27', 'f_28']
