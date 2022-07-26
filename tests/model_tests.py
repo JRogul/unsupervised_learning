b0cdc0f6e44d2fe078b0626c6f4adcccbc6f5bba
JRogulski <jakubrogulski01@gmail.com>
2022-07-26 16:58:10 +0200

    few more comments

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -4,6 +4,7 @@ import joblib
 from src import config
 from src import data_visualization
 train_scaled = data_proccesing.train_scaled
+
 model_bgm = joblib.load('../models/BayesianGaussianMixture')
 
 X, y = train.predict(model_bgm,train_scaled)
